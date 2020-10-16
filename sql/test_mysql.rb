1bdd8b8e76ff579c65aeaa7bd5dd9565b3f6537b
Bin Le <binle@Mbp16.local>
2020-10-15 21:23:07 -0400

    minor comment added

--- sql/test_mysql.rb
+++ sql/test_mysql.rb
@@ -2,6 +2,15 @@
 require 'rubygems'
 require 'mysql'
 
+# Optional: use a `db_conn.yml` configuration file
+# assume yaml file has following keys
+# mysql:
+#     host:
+#     database:
+#     username:
+#     password:
+#
+
 db = Mysql.connect('localhost', 'root', '', 'test')
 
 rs = db.query("select 1")
